c1f79b0a40c172eb3c39604293d57ddc18e3f311
Khemi <49807719+khemichew@users.noreply.github.com>
2021-01-18 06:41:35 +0800

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,7 @@
+function alert1(){
+	alert("Thank you for subscribing to Sunway Tech Club's newsletter!")
+}
+
+function alert2(){
+	alert("Thank you for submitting your feedback/enquiry! We will get back to you by email within one or two days.")
+}
